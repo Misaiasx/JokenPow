7118a7f9c43a1663c4358ecdf5410db394841406
Misaiasx <mizaiascorrea@gmail.com>
2024-02-29 19:23:39 -0300

    JokenPô

---
+++ scripts.js
@@ -0,0 +1,49 @@
+const result = document.querySelector(".result");
+const humanScore = document.querySelector("#human-score")
+const machineScore = document.querySelector("#machine-score")
+
+let humanScoreNumber = 0
+let machineScoreNumber = 0
+/*
+humanScoreNumber ->  Camel Case
+GAME_OPTIONS      -> Snake 
+*/
+
+const playHuman = (humanChoice) => {
+  // console.log(humanChoice)
+  // console.log('retorno'+ playMachine())
+  playTheGame(humanChoice, playMachine());
+};
+
+const playMachine = () => {
+  const choices = ["rock", "paper", "scissors"];
+  const randomNumber = Math.floor(Math.random() * 3);
+
+  // console.log(randomNumber)
+  return choices[randomNumber];
+};
+
+const playTheGame = (human, machine) => {
+  console.log("Humano:" + human + "  Maquina:" + machine);
+
+  if (human === machine) {
+    result.innerHTML = "Deu Empate!";
+  } else if (
+    (human === "paper" && machine === "rock") ||
+    (human === "rock" && machine === "scissors") ||
+    (human === "scissors" && machine === "paper")
+  ) {
+    humanScoreNumber ++
+    humanScore.innerHTML = humanScoreNumber
+    result.innerHTML = "Você Ganhou!";
+  } else {
+    machineScoreNumber ++ 
+    machineScore.innerHTML =machineScoreNumber
+    result.innerHTML = "Você Perdeu para Alexa!";
+  }
+};
+
+/*
+  papel-> pedra -> tesoura
+
+  */
